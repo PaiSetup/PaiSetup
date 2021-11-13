70cb736faa15ea0cb29be0f9b08a883f30b699dc
MaciejDziuban <dziuban.maciej@gmail.com>
2021-11-13 01:50:43 +0100

    Add memory dialog

--- steps/dwm/dwmblocks/resources.sh
+++ steps/dwm/dwmblocks/resources.sh
@@ -1,3 +1,5 @@
+[ "$BUTTON" = "1" ] && notify-send "🧠 Memory hogs" "$(ps axch -o cmd:15,%mem --sort=-%mem | head)"
+
 #free -h | awk '/^Mem/ { print $3"/"$2 }' | sed s/i//g
 free | awk 'NR == 2 {printf("%.2f%% of "), $3/$2*100}'
 free -h | awk 'NR == 2 {printf ($2)}'
