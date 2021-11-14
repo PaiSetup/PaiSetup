ddb94fbc29004d3013f0b22d02b4d5ddb730e667
MaciejDziuban <dziuban.maciej@gmail.com>
2021-11-14 20:31:46 +0100

    Remove unneeded code

--- root_setup.sh
+++ root_setup.sh
@@ -15,8 +15,6 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 loadkeys pl
-localectl set-x11-keymap pl
-setxkbmap pl
 echo pl > /etc/vconsole.conf
 
 echo "Configuring root passwd"
