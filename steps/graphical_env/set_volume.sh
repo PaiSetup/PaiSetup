8862e4ad8b88fe4805bfe7dce9eac44dc18f537a
MaciejDziuban <dziuban.maciej@gmail.com>
2021-11-23 21:36:11 +0100

    Add volume controls

---
+++ steps/graphical_env/set_volume.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case "$1" in
+  "0")
+    pamixer --toggle-mute
+    ;;
+  "1") 
+    pamixer --decrease 1 --unmute
+    ;;
+  "2")
+    pamixer --increase 1 --unmute
+    ;;
+    *)
+    exit 1
+    ;;
+esac
+
+pkill -RTMIN+12 dwmblocks
