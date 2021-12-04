b2a4a33050da6c5d6225e2ddb5fb295382901310
MaciejDziuban <dziuban.maciej@gmail.com>
2021-12-05 00:33:12 +0100

    Improve audio dwmblocks module
    - run pulsemixer truly in background (right lick)
    - print info on left click
    - switch audio device on middle click
    - add device icon

--- Ricing.md
+++ Ricing.md
@@ -2,3 +2,4 @@
 1. DWM patches organized https://coggle.it/diagram/X9IiSSM6PTWOM9Wz/t/dwm-patches-last-tallied-2021-09-28
 2. Awesome fonts (monochromatic) - https://fontawesome.com/v5/cheatsheet
 3. Colored emoji - https://www.emojicopy.com/
+4. Terminal colors - https://terminal.sexy
--- steps/dwm/dwmblocks/volume.sh
+++ steps/dwm/dwmblocks/volume.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-[ "$BUTTON" = "3" ] && $TERMINAL pulsemixer &
+[ "$BUTTON" = "1" ] && {
+    notify-send "🎛️ 1Current sink" "$(pamixer --get-default-sink | grep -oE "\"[^\"]+\"$" | tr -d \")"
+    notify-send "🎛️ Available sinks" "$(pamixer --list-sinks | grep -oE "\"[^\"]+\"$" | tr -d \" | sed "s/^/ - /g")"
+}
+[ "$BUTTON" = "2" ] && {
+    current_sink="$(pamixer --get-default-sink | grep -oE ^[0-9]+)"
+    next=$((pamixer --list-sinks; pamixer --list-sinks) | # List sinks two times, so the list wraps around after last sink
+        grep -vE "Sinks|Built-in"                       | # Remove unneeded lines
+        grep -m1 -A1 -E "^$current_sink"                | # Find first occurrence of our current sink and also print one line below
+        tail -1                                         | # Take only line below, which is our next sink
+        cut -d' ' -f1)                                    # Extract index of the sink
+    [ -z "$next" ] && next="0"
+    pacmd set-default-sink "$next"
+}
+[ "$BUTTON" = "3" ] && {
+    $TERMINAL pulsemixer >/dev/null 2>/dev/null &
+}
 
 is_enabled=$(amixer get Master | grep -c "\[on\]")
 volume=$(amixer get Master | grep -E "[0-9]+%" -o | sed 's/%//g' | head -1 | tr -d '\n')
@@ -16,6 +32,10 @@ else
     fi
 fi
 
+device_icon=""
+(pamixer --get-default-sink | grep -q  "USB") && device_icon=""
+(pamixer --get-default-sink | grep -qE "HDMI|VGA") && device_icon=""
+
 $LINUX_SETUP_ROOT/steps/dwm/dwmblocks/bg_helper.sh start 1
-printf "$icon %3s%%" $volume
+printf "$device_icon $icon %3s%%" $volume
 $LINUX_SETUP_ROOT/steps/dwm/dwmblocks/bg_helper.sh end 1
