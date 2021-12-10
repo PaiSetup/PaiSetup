4ccdd175d35cc5598150e7026b8b37a9c05c79e6
MaciejDziuban <dziuban.maciej@gmail.com>
2021-12-10 20:03:09 +0100

    Use proper background color for tray

--- steps/dwm/dwmblocks/tray.sh
+++ steps/dwm/dwmblocks/tray.sh
@@ -4,7 +4,7 @@ $LINUX_SETUP_ROOT/steps/dwm/dwmblocks/bg_helper.sh start 0
 
 [ "$BUTTON" = "1" ] && {
     if [ -z "$(pgrep stalonetray)" ]; then
-        stalonetray >/dev/null 2>/dev/null &
+        xrdb -query | grep dwm.normbgcolor | cut -f2 | xargs stalonetray -bg >/dev/null 2>/dev/null &
     else
         pgrep stalonetray | xargs -l1  kill -9
     fi
