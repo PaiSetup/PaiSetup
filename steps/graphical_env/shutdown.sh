cbbe377a985e774afd0612484378b619b20d8199
MaciejDziuban <dziuban.maciej@gmail.com>
2021-12-12 14:22:11 +0100

    Kill all user processes on "Exit GUI"

--- steps/graphical_env/shutdown.sh
+++ steps/graphical_env/shutdown.sh
@@ -4,5 +4,5 @@ selection=$(printf "Cancel\nShutdown\nReboot\nExit GUI" | dmenu)
 case $selection in
     Shutdown) shutdown now ;;
     Reboot) reboot ;;
-    "Exit GUI") pkill Xorg ;;
+    "Exit GUI") kill -9 -1 ;;
 esac
