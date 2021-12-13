10591b0fe2702c0492d9793d0d5506e155555c0e
MaciejDziuban <dziuban.maciej@gmail.com>
2021-12-13 01:25:30 +0100

    Add calendar on statusbar click

--- steps/dwm/dwm.py
+++ steps/dwm/dwm.py
@@ -56,6 +56,7 @@ class DwmStep(GraphicalEnvStep):
                 "xorg-xsetroot",
                 "xorg-setxkbmap",
                 "stalonetray",
+                "yad",
             ]
         )
 
--- steps/dwm/dwmblocks/date.sh
+++ steps/dwm/dwmblocks/date.sh
@@ -4,4 +4,12 @@ $LINUX_SETUP_ROOT/steps/dwm/dwmblocks/bg_helper.sh start 1
 
 printf " %s" "$(date '+%d %b %y  %R')"
 
+[ "$BUTTON" = "1" ] && {
+    if pgrep "yad --calendar" -f >/dev/null 2>&1; then
+        pkill "yad --calendar" -f
+    else
+        yad --calendar --posx=-10 --posy=50 --no-buttons >/dev/null 2>&1 &
+    fi
+}
+
 $LINUX_SETUP_ROOT/steps/dwm/dwmblocks/bg_helper.sh end 1
