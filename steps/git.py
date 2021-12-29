f95138ce70ffeb574db2c8853a2e6c4235cb1dfb
MaciejDziuban <dziuban.maciej@gmail.com>
2021-12-29 17:47:50 +0100

    Bash git completion

--- steps/git.py
+++ steps/git.py
@@ -2,6 +2,7 @@ from steps.step import Step
 from utils import command
 import os
 from pathlib import Path
+from steps.dotfiles import FileType
 
 
 class GitStep(Step):
@@ -10,6 +11,14 @@ class GitStep(Step):
 
     def setup_required_packages(self, packages_step):
         packages_step.add_packages("git")
+
+    def setup_required_dotfiles(self, dotfiles_step):
+        dotfiles_step.add_dotfile_section(
+            ".bashrc",
+            "Enable git commands completion",
+            [". /usr/share/git/completion/git-completion.bash"],
+            file_type=FileType.Bash,
+        )
 
     def _perform_impl(self):
         gitconfig_dir = Path(os.environ["HOME"]) / ".config" / "git"
