230863386f2e003736edaa3bdf8248f98741265e
MaciejDziuban <dziuban.maciej@gmail.com>
2022-07-12 23:17:44 +0200

    Add clangformat extension

--- steps/vscode/vscode.py
+++ steps/vscode/vscode.py
@@ -43,6 +43,7 @@ class VscodeStep(Step):
         extensions.append("ms-vscode.cmake-tools")
         extensions.append("hbenl.test-adapter-converter")
         extensions.append("hbenl.vscode-test-explorer")
+        extensions.append("xaver.clang-format")
 
         # Python
         log("Installing python formatters")
