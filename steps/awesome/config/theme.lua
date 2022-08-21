5af0f920db967f9fdfea18ee35611450843a63e9
MaciejDziuban <dziuban.maciej@gmail.com>
2022-08-21 23:35:19 +0200

    Cleanup theme.lua

--- steps/awesome/config/theme.lua
+++ steps/awesome/config/theme.lua
@@ -1,19 +1,16 @@
----------------------------
--- Default awesome theme --
----------------------------
-
+----------------------------------------------------------------------------------- Module imports
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
-
 local gfs = require("gears.filesystem")
 local gears = require("gears")
 local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "sans 8"
 
+
+----------------------------------------------------------------------------------- Load XResources
 xrdb = xresources.get_current_theme()
 color_theme = xrdb.color1
 color_gray_light = xrdb.color2
@@ -22,6 +19,21 @@ color_font = xrdb.color4
 color_urgent = "00ff00" -- I don't know where I'll see this color. Make it something flashy, so I'll notice
 color_minimze = "00ffff" -- I don't know where I'll see this color. Make it something flashy, so I'll notice
 
+
+
+----------------------------------------------------------------------------------- Custom values for our setup (used in rc.lua or its sub-scripts)
+theme.widgets_padding = 3
+theme.widgets_margin = 4
+theme.widgets_group_margin = 6
+theme.bg_normal2 = color_gray_dark
+theme.color_theme = color_theme
+theme.color_gray_light = color_gray_light
+theme.color_gray_dark = color_gray_dark
+
+
+
+----------------------------------------------------------------------------------- General config
+theme.font          = "sans 8"
 
 theme.fg_normal     = color_font
 theme.fg_focus      = color_font
@@ -42,19 +54,7 @@ theme.border_marked = "#000000"
 
 
 
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- prompt_[fg|bg|fg_cursor|bg_cursor|font]
--- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
-
+----------------------------------------------------------------------------------- Tasklist
 theme.tasklist_fg_normal   = color_theme
 theme.tasklist_fg_focus    = color_theme
 theme.tasklist_fg_urgent   = color_theme
@@ -66,7 +66,9 @@ theme.tasklist_bg_minimize = color_gray_light
 theme.tasklist_shape = gears.shape.rounded_rect
 theme.tasklist_disable_icon = false
 
--- Taglist config
+
+
+----------------------------------------------------------------------------------- Taglist
 theme.taglist_fg_empty = color_theme
 theme.taglist_fg_focus = color_gray_light
 theme.taglist_fg_occupied = theme.taglist_fg_empty
@@ -77,7 +79,9 @@ theme.taglist_bg_volatile = color_urgent
 theme.taglist_bg_urgent = color_urgent
 theme.taglist_shape = gears.shape.rounded_rect
 
--- Hotkeys help popup config
+
+
+----------------------------------------------------------------------------------- Hotkeys popup
 theme.hotkeys_bg = theme.bg_normal
 theme.hotkeys_fg = theme.fg_normal
 theme.hotkeys_shape = gears.shape.rounded_rect
@@ -85,58 +89,26 @@ theme.hotkeys_modifiers_fg = color_theme
 theme.hotkeys_label_fg = color_font
 theme.hotkeys_group_margin = dpi(30)
 
+
+
+----------------------------------------------------------------------------------- Notifications
+-- TODO
 -- Variables set for theming notifications:
 -- notification_font
 -- notification_[bg|fg]
 -- notification_[width|height|margin]
 -- notification_[border_color|border_width|shape|opacity]
 
--- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
-theme.menu_height = dpi(15)
-theme.menu_width  = dpi(100)
 
--- Wibar settings
+
+----------------------------------------------------------------------------------- Wibar
 theme.wibar_border_width = dpi(10)
 theme.wibar_bg = "#00000000"
 theme.wibar_height = dpi(30)
 
 
--- You can add as many variables as
--- you wish and access them by using
--- beautiful.variable in your rc.lua
---theme.bg_widget = "#cc0000"
 
--- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
-
-theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
-
--- You can use your own layout icons like this:
+----------------------------------------------------------------------------------- Layout icons
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
 theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
 theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
@@ -154,27 +126,12 @@ theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
 theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
 theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
 
--- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(
-    theme.menu_height, theme.bg_focus, theme.fg_focus
-)
 
+
+----------------------------------------------------------------------------------- Rest
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
 
 theme.rounded = dpi(15)
-
--- Custom values for our setup
-theme.widgets_padding = 3
-theme.widgets_margin = 4
-theme.widgets_group_margin = 6
-theme.bg_normal2 = color_gray_dark
-theme.color_theme = color_theme
-theme.color_gray_light = color_gray_light
-theme.color_gray_dark = color_gray_dark
-
 return theme
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
-
