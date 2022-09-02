fbc145020589aded9a9dac7e4c0ee1201b0e68b1
MaciejDziuban <dziuban.maciej@gmail.com>
2022-09-02 19:52:32 +0200

    Add icons to shutdown popup

--- steps/awesome/config/utils/widget.lua
+++ steps/awesome/config/utils/widget.lua
@@ -54,36 +54,51 @@ local _shutdown_popup_data = nil
 local function shutdown_popup()
     -- Create the popup and all its data, if this is the first time we call it
     if _shutdown_popup_data == nil then
-        local function create_button(text, callback)
+        local function create_button(icon, caption, callback)
             local widget = {
                 widget = wibox.widget.textbox,
-                text = text,
+                text = icon,
                 forced_height = 100,
                 forced_width = 100,
                 align = "center",
                 valign = "center",
+                font = "sans 50",
             }
             widget = widget_wrappers.margin(widget, 10)
             widget = widget_wrappers.border(widget, gears.shape.rounded_rect, 3, beautiful.color_gray_light)
             widget.set_focused = function(self, value)
                 if value then
-                    self.shape_border_color = beautiful.color_theme
+                    self.bg = beautiful.color_theme
                 else
-                    self.shape_border_color = beautiful.color_gray_light
+                    self.bg = beautiful.color_gray_light 
                 end
             end
             widget.callback = callback
+            widget.caption = caption
+            return widget
+        end
+
+        local function create_selected_caption()
+            local widget = wibox.widget.textbox()
+            widget.align = "center"
+            widget.valign = "center"
+            widget.font = "sans 20"
             return widget
         end
 
         local function create_main_widget(buttons)
-            local widget = gears.table.join(
-                {
-                    layout = wibox.layout.fixed.horizontal,
-                    spacing = 10,
-                },
-                _shutdown_popup_data.buttons
-            )
+            local widget = {
+                layout = wibox.layout.fixed.vertical,
+                spacing = 10,
+                gears.table.join(
+                    {
+                        layout = wibox.layout.fixed.horizontal,
+                        spacing = 10,
+                    },
+                    _shutdown_popup_data.buttons
+                ),
+                _shutdown_popup_data.selected_caption
+            }
             widget = widget_wrappers.margin(widget, 5)
             return widget
         end
@@ -91,16 +106,18 @@ local function shutdown_popup()
         _shutdown_popup_data = {}
         _shutdown_popup_data.selection = 0
         _shutdown_popup_data.buttons = {
-            create_button("Cancel",   function() end ),
-            create_button("Shutdown", function() awful.spawn("shutdown now") end),
-            create_button("Reboot",   function() awful.spawn("reboot")       end),
-            create_button("Exit GUI", function() awful.spawn("pkill awesome")       end),
+            create_button("", "Cancel",   function()                               end),
+            create_button("", "Shutdown", function() awful.spawn("shutdown now")   end),
+            create_button("", "Reboot",   function() awful.spawn("reboot")         end),
+            create_button("", "Exit GUI", function() awful.spawn("pkill awesome")  end),
         }
+        _shutdown_popup_data.selected_caption = create_selected_caption()
         _shutdown_popup_data.refresh = function(self)
             for _, button in pairs(self.buttons) do
                 button:set_focused(false)
             end
             self.buttons[self.selection]:set_focused(true)
+            self.selected_caption.text = self.buttons[self.selection].caption
         end
 
         _shutdown_popup_data.popup = awful.popup {
