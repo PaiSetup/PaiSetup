55db4ff2a7ee98b6567dea0895f9c35e01e3c808
MaciejDziuban <dziuban.maciej@gmail.com>
2022-09-10 15:01:25 +0200

    Add error check to get_unmatching_packages

--- steps/bg_checker/check_unmatching_packages.sh
+++ steps/bg_checker/check_unmatching_packages.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 . ~/.profile # for get_unmatching_packages command
 
-packages="$(get_unmatching_packages | grep -E "<|>")"
-if [ -n "$packages" ]; then
-    echo "$(echo "$packages" | wc -l) packages do not match with LinuxSetup"
+packages="$(get_unmatching_packages)"
+if [ $? = 0 ]; then
+    packages="$(echo "$packages" | grep -E '<|>')"
+    if [ -n "$packages" ]; then
+        echo "$(echo "$packages" | wc -l) packages do not match with LinuxSetup"
+    fi
+else
+    echo "Could not gather LinuxSetup packages"
 fi
