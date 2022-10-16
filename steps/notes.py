d3ab9a80955f8be93d72afa3690fb05167280a7d
MaciejDziuban <dziuban.maciej@gmail.com>
2022-10-16 14:10:27 +0200

    Add notes path env var

--- steps/notes.py
+++ steps/notes.py
@@ -13,10 +13,11 @@ class NotesStep(Step):
 
     def express_dependencies(self, dependency_dispatcher):
         dependency_dispatcher.add_packages("obsidian")
-        dependency_dispatcher.add_keybindings(
-            KeyBinding('t').mod().shift().execute("obsidian e")
-        )
+        dependency_dispatcher.add_keybindings(KeyBinding("t").mod().shift().execute("obsidian e"))
         dependency_dispatcher.set_folder_icon(self._notes_dir, "notes")
+
+    def register_env_variables(self):
+        self._env.set("NOTES_PATH", self._notes_dir)
 
     def perform(self):
         ext.download(
@@ -24,4 +25,10 @@ class NotesStep(Step):
             "master",
             self._notes_dir,
             fetch=self.fetch_git,
+        )
+
+        self._file_writer.write_section(
+            ".config/LinuxSetup/xinitrc_base",
+            "My personal notes",
+            [f'export NOTES_PATH="{self._notes_dir}"'],
         )
