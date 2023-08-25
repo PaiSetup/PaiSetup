21613ec48da47db5aee6a2f73f2b80caf7966020
MaciejDziuban <dziuban.maciej@gmail.com>
2023-08-25 22:58:18 +0200

    Remove startup entries

--- steps/windows/startup.py
+++ steps/windows/startup.py
@@ -1,6 +1,7 @@
 from steps.step import Step
 from utils import command
 from utils.log import log
+from utils.windows_registry import *
 
 
 class StartupStep(Step):
@@ -10,6 +11,7 @@ class StartupStep(Step):
     def perform(self):
         self._remove_scheduled_tasks()
         self._remove_services()
+        self._remove_startup_entries()
 
     def _remove_scheduled_tasks(self):
         tasks = [
@@ -32,3 +34,26 @@ class StartupStep(Step):
         powershell_script = [f'Stop-Service "{service}"; Set-Service "{service}" -StartupType Disabled' for service in services]
         log("Removing services")
         command.run_powershell_command(powershell_script)
+
+    def _remove_startup_entries(self):
+        # TODO these should ideally be added by other steps through dependency dispatcher
+        log("Removing startup entries")
+        self._remove_startup_entry("BCClipboard")
+        self._remove_startup_entry("CCleaner Smart Cleaning")
+        self._remove_startup_entry("Discord")
+        self._remove_startup_entry("OneDrive")
+        self._remove_startup_entry("Steam")
+        self._remove_startup_entry("SunJavaUpdateSched")
+
+    def _remove_startup_entry(self, name):
+        for hive in [HKCU, HKLM]:
+            for bitness in ["", "WOW6432Node\\"]:
+                for subkey in [
+                    "Run",
+                    "RunOnce",
+                    "Explorer\\StartupApproved\\Run",
+                    "Explorer\\StartupApproved\\Run32",
+                    "Explorer\\StartupApproved\\StartupFolder",
+                ]:
+                    key = rf"SOFTWARE\{bitness}Microsoft\Windows\CurrentVersion\{subkey}"
+                    delete_registry_value(hive, key, name)
