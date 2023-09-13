ca336b601c8516b8d912971eee39d8c5b6afb213
MaciejDziuban <dziuban.maciej@gmail.com>
2023-09-13 20:53:24 +0200

    Move get_unmatching_packages into PaiSetup

--- steps/linux/check_mate/check_unmatching_packages.sh
+++ steps/linux/check_mate/check_unmatching_packages.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
-. ~/.profile # for get_unmatching_packages command
 
-packages="$(get_unmatching_packages)" # TODO move this into PaiSetup
+get_unmatching_packages() {
+    file_expected="$(mktemp --suffix=".expected")"
+    file_actual="$(mktemp --suffix=".actual")"
+
+    printf "Gathering expected packages in $file_expected... "
+    expected_packages="$($PAI_SETUP_ROOT/setup.py --list_packages)"
+    if [ $? != 0 ]; then
+        return 1
+    fi
+    echo "$expected_packages" | sort | uniq | tee "$file_expected" | wc -l
+
+    printf "Gathering actual packages in $file_actual... "
+    yay -Qeq | sort | tee "$file_actual" | wc -l
+
+    echo "diff \"$file_expected\" \"$file_actual\""
+    diff "$file_expected" "$file_actual" --side-by-side --color=auto --suppress-common-lines
+
+    echo "Removing tmp files"
+    rm "$file_actual"
+    rm "$file_expected"
+}
+
+packages="$(get_unmatching_packages)"
 if [ $? = 0 ]; then
     packages="$(echo "$packages" | grep -E '<|>')"
     if [ -n "$packages" ]; then
