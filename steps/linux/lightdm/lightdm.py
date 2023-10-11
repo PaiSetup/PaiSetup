9cc56087cca68e2d75d9faedca1e3cffa073440b
MaciejDziuban <dziuban.maciej@gmail.com>
2023-10-11 20:22:14 +0200

    Optimie LightDmStep

--- steps/linux/lightdm/lightdm.py
+++ steps/linux/lightdm/lightdm.py
@@ -15,12 +15,26 @@ class LightDmStep(Step):
         dependency_dispatcher.register_homedir_file(".dmrc")
 
     def perform(self):
-        # Enable service
+        self._enable_service()
+        self._configure()
+
+    def _enable_service(self):
+        # Check, if we can skip
+        try:
+            out = command.run_command("systemctl status display-manager", stdout=command.Stdout.return_back())
+            out = out.splitlines()
+            out = out[0]
+            if "lightdm.service" in out:
+                return
+        except command.CommandError:
+            # No display manager detected
+            pass
+
         service_name = "lightdm.service"
         self._logger.log(f"Enabling {service_name}")
         command.run_command(f"sudo systemctl enable {service_name}")
 
-        # Configure
+    def _configure(self):
         current_step_dir = Path(__file__).parent
         config_path = "/usr/share/lightdm"
         self._logger.log(f"Setting up config files in {config_path}")
