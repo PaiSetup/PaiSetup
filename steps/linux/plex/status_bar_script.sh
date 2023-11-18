dde5e78a150843a0990b43a17f765a95242c1147
MaciejDziuban <dziuban.maciej@gmail.com>
2023-11-18 16:32:10 +0100

    Improve plex status bar script

--- steps/linux/plex/status_bar_script.sh
+++ steps/linux/plex/status_bar_script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 service=plexmediaserver.service
+url=http://localhost:32400/
 systemctl is-active --quiet $service
 is_not_running=$? # 0 if running, otherwise not running
 show_status=0
@@ -10,17 +11,24 @@ if [ "$BUTTON" = "$BUTTON_INFO" ]; then
     show_status=1
 fi
 if [ "$BUTTON" = "$BUTTON_ACTION" ]; then
+
     if [ "$is_not_running" = 0 ]; then
         sudo systemctl stop $service && is_not_running=1
     else
-        sudo systemctl start $service && is_not_running=0 || sudo systemctl stop $service
+        if sudo systemctl start $service; then
+            is_not_running=0
+            show_status=1
+            (sleep 2; $BROWSER $url) & disown
+        else
+            notify-send "Failed to start service $service. Wait for a few seconds and try again."
+            sudo systemctl stop $service
+        fi
     fi
-    show_status=1
 fi
 
 if [ "$show_status" != 0 ]; then
     if [ "$is_not_running" = 0 ]; then
-        notify-send "Plex is running" "Service $service is running. Access Plex at http://localhost:32400/"
+        notify-send "Plex is running" "Service $service is running. Access Plex at $url"
     else
         notify-send "Plex is not running" "Service $service is stopped"
     fi
