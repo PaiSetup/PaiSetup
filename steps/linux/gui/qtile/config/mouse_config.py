088305feea96e2c08529ffcf55fa24bd7ee283b9
MaciejDziuban <dziuban.maciej@gmail.com>
2024-07-08 23:38:00 +0200

    Add unfloating mouse shortcuts

--- steps/linux/gui/qtile/config/mouse_config.py
+++ steps/linux/gui/qtile/config/mouse_config.py
@@ -4,10 +4,17 @@ from libqtile.lazy import lazy
 from utils.modkeys import *
 
 
+@lazy.function
+def disable_all_window_floating(qtile):
+    for window in qtile.current_group.windows:
+        window.disable_floating()
+
+
 class MouseConfig:
     def __init__(self):
         self.mouse = [
             Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
             Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-            Click([mod], "Button2", lazy.window.bring_to_front()),
+            Click([mod], "Button2", lazy.window.disable_floating()),
+            Click([mod, shift], "Button2", disable_all_window_floating()),
         ]
