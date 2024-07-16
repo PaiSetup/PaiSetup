f4bb5b25ef67a9cd7fc0e64a4775c754ceff2b4f
MaciejDziuban <dziuban.maciej@gmail.com>
2024-07-16 13:06:06 +0200

    Ignore errors when restarting AwesomeWM, they are expected
    See https://www.reddit.com/r/awesomewm/comments/a3xug0/how_to_restart_awesome_from_the_command_line/

--- steps/linux/gui/scripts/restart_wm.sh
+++ steps/linux/gui/scripts/restart_wm.sh
@@ -2,6 +2,6 @@
 
 case "$WM" in
     "dwm") kill -TERM  $(pgrep ^dwm$) ;;
-    "awesome") awesome-client "awesome.restart()" ;;
+    "awesome") awesome-client "awesome.restart()" 2>/dev/null || exit 0 ;;
     "qtile") qtile cmd-obj -o cmd -f restart ;;
 esac
