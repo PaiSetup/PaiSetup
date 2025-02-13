3897564ec7c5365ee2633ad510cb7d07ebc1c1d6
MaciejDziuban <dziuban.maciej@gmail.com>
2025-02-13 18:05:39 +0100

    Remove code for save/restore clipboard. I give up - it cannot be done

--- steps/linux/gui/scripts/get_thunar_cwd.sh
+++ steps/linux/gui/scripts/get_thunar_cwd.sh
@@ -7,18 +7,11 @@ if [ "$focused_window_class" != "Thunar" ];  then
     exit 1
 fi
 
-# Save clipboard
-# clipboard="$(xclip -o)"
-
 # Activate Thunar window and copy the current path
 xdotool windowactivate --sync $focused_window_id || exit 1
 sleep 0.05 # this micro-sleep is really needed
 xdotool key --clearmodifiers ctrl+l ctrl+c || exit 1
 cwd="$(xclip -o)" || exit 1
-
-# Restore clipboard
-# TODO why does this lag?
-# echo "$clipboard" | xclip -selection clipboard
 
 # Return current path to stdout
 if [ -z "$cwd" ]; then
