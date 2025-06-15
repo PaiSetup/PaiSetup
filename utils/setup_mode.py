9b78663f5c6d245936587e0c0c70b8fded1ccbff
MaciejDziuban <dziuban.maciej@gmail.com>
2025-06-15 20:25:14 +0200

    Make SetupMode more robust

--- utils/setup_mode.py
+++ utils/setup_mode.py
@@ -17,14 +17,36 @@ class SetupMode(enum.Enum):
         try:
             with open(lastmode_file, "r") as file:
                 lastmode = SetupMode(file.readline().strip())
+
+            if not lastmode._is_compatible():
+                lastmode = SetupMode._get_default_mode()
+
+            return lastmode
+
         except (FileNotFoundError, ValueError) as e:
-            if OperatingSystem.current().is_windows():
-                lastmode = SetupMode.windows
-            elif LinuxDistro.current().is_debian_like():
-                lastmode = SetupMode.debian_casual
-            else:
-                lastmode = SetupMode.arch
-        return lastmode
+            return SetupMode._get_default_mode()
+
+    @staticmethod
+    def _get_default_mode():
+        if OperatingSystem.current().is_windows():
+            return SetupMode.windows
+        elif LinuxDistro.current().is_debian_like():
+            return SetupMode.debian_casual
+        elif LinuxDistro.current().is_arch_like():
+            return SetupMode.arch
+        else:
+            raise ValueError("Unknown system")
+
+    def _is_compatible(self):
+        match self:
+            case SetupMode.arch:
+                return OperatingSystem.current().is_linux() and LinuxDistro.current().is_arch_like()
+            case SetupMode.debian_casual | SetupMode.debian_work:
+                return OperatingSystem.current().is_linux() and LinuxDistro.current().is_debian_like()
+            case SetupMode.windows:
+                return OperatingSystem.current().is_windows()
+            case _:
+                raise ValueError("Invalid SetupMode")
 
     def save_last_mode(self, root_dir):
         lastmode_file = root_dir / ".lastmode"
