075e2136d9eee1dedded4ff337b48c5c0352774f
MaciejDziuban <dziuban.maciej@gmail.com>
2025-12-09 18:29:20 +0100

    Improve subvaults script

--- steps/linux/notes/verify_subvaults_coherency.sh
+++ steps/linux/notes/verify_subvaults_coherency.sh
@@ -13,9 +13,16 @@ meaningful_files=(
 
 result=0
 while read -r sub_vault; do
+    # Skip empty directories
+    if ! find "$sub_vault" -mindepth 1 -print -quit | grep -q .; then
+        continue
+    fi
+
     sub_valut_name="${sub_vault##*/}"
 
-    for file in ${meaningful_files[*]}; do
+    # Perform the action on all files that we care about: either copy from parent vault
+    # or complain that they are different.
+    for file in "${meaningful_files[@]}"; do
         if [ "$perform_copy" = "1" ]; then
             cp "$NOTES_PATH/$file" "$sub_vault/$file"
         else
