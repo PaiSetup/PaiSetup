907cd01654aee86e791d900a813dde3231a596a9
MaciejDziuban <dziuban.maciej@gmail.com>
2026-02-03 23:28:44 +0100

    Setup Dush on Windows

--- steps/windows/dush.py
+++ steps/windows/dush.py
@@ -8,9 +8,12 @@ class DushStep(DushStepBase):
 
     def pull_dependencies(self, dependency_dispatcher):
         known_folders = dependency_dispatcher.get_known_folders()
-        root_dir = known_folders[KnownFolder.Dush]
+
         self._projects_dir = known_folders[KnownFolder.Projects]
-        self.setup_dush_root_dir(root_dir)
+        self._documents_dir = known_folders[KnownFolder.Documents]
+        self._dush_dir = known_folders[KnownFolder.Dush]
+
+        self.setup_dush_root_dir(self._dush_dir)
 
     def perform(self):
         super().perform()
@@ -23,5 +26,18 @@ class DushStep(DushStepBase):
                 f'export DUSH_ENABLE_AUTOLOAD="1"',
                 ". $DUSH_PATH/framework/frontend.bash",
                 ". $DUSH_PATH/projects/bashies/main.sh",
+            ],
+        )
+
+        powershell_profile = self._documents_dir / "WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+        self._file_writer.write_section(
+            powershell_profile,
+            "Developer scripts",
+            [
+                f'$env:PYTHONPATH = "{self._dush_dir};$env:PYTHONPATH"',
+                f'$env:DUSH_WORKSPACE = "{self._projects_dir}"',
+                f'$env:DUSH_PATH = "{self._dush_dir}"',
+                "$env:DUSH_ENABLE_AUTOLOAD = $true",
+                "Import-Module $env:DUSH_PATH/projects/bashies/main.ps1",
             ],
         )
