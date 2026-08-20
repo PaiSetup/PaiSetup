a69ea9f7522b3840b65d9e0be007d45635ab8b76
MaciejDziuban <dziuban.maciej@gmail.com>
2026-08-20 09:22:40 +0200

    Add utils.windows.add_to_path

--- utils/windows/env.py
+++ utils/windows/env.py
@@ -11,3 +11,13 @@ def set_user_env_variable(name, value):
 
 def set_machine_env_variable(name, value):
     run_powershell_command(f'[Environment]::SetEnvironmentVariable("{name}", "{value}", "Machine")')
+
+
+def add_to_path(path, system=False):
+    scope = "Machine" if system else "User"
+    current_path = run_powershell_command(f'[Environment]::GetEnvironmentVariable("PATH", "{scope}")', stdout=Stdout.return_back()).stdout.strip()
+    current_entries = [entry.lower() for entry in current_path.split(";")]
+    if path.lower() in current_entries:
+        return
+    new_path = f"{current_path};{path}"
+    run_powershell_command(f'[Environment]::SetEnvironmentVariable("PATH", "{new_path}", "{scope}")')
